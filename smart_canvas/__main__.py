c0f3ad4cedd0b417ae74672fe315c38677d94c1f
Jarkko Kotaniemi <jarkko.kotaniemi@vtt.fi>
2021-10-30 18:37:58 +0300

    ISSUE-21: background is set before taking pictures

--- smart_canvas/__main__.py
+++ smart_canvas/__main__.py
@@ -9,19 +9,26 @@ import cv2
 
 # Internal packages
 from .core import process
+from .core import create_background
 
 def main():
     device = cv2.VideoCapture(0)
-
+    count = 0
     while(True):
         res, frame = device.read()
         if not res:
             break
-        processed_frame = process(frame)
-        cv2.imshow('output', processed_frame)
+        if count < 50:  # make a reference background out of 50 frames
+            create_background(frame)
+        cv2.imshow('output', frame)
         key = cv2.waitKey(1)
+        if key == ord('a'):  # take a picture and process it
+            processed_frame = process(frame)
+            cv2.imshow('output', processed_frame)
+            cv2.waitKey(0)
         if key == ord('q'): 
             break
+        count += 1
 
     if res:    
         cv2.destroyWindow('output')
