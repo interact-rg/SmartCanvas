02a9b79db60cdb605f985bf741db86ed049e1e62
Jarkko Kotaniemi <jarkko.kotaniemi@vtt.fi>
2021-11-01 13:23:19 +0200

    ISSUE-31: change core.py functions into a class

--- smart_canvas/core.py
+++ smart_canvas/core.py
@@ -4,46 +4,40 @@
 
 # External packages
 import cv2
-import numpy as np
 # Internal packages
 from . import filtering
 from . import background
 from . import gestureDetection
 
-current_filter = filtering.catalog[next(filtering.carousel)]
-framecount = 0
-pictureframe = None
-delaycounter = 0
-filtercounter = 0
 
-def create_background(frame):
-    background.foregroundMask(frame)
+class FrameProcessor:
+    def __init__(self):
+        self.current_filter = filtering.catalog[next(filtering.carousel)]
+        self.framecount = 0
+        self.pictureframe = None
+        self.delaycounter = 0
+        self.filtercounter = 0
 
-def process(frame):
-    global framecount
-    global pictureframe
-    global delaycounter
-    global current_filter
-    global filtercounter
-    if delaycounter > 0:
-        delaycounter -= 1
-        return pictureframe
-    if filtercounter > 0:
-        filtercounter -= 1
-        cv2.putText(frame, current_filter.__name__, (45, 375), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 5)
+    def process(self, frame):
+        if self.delaycounter > 0:  # show the filtered picture
+            self.delaycounter -= 1
+            return self.pictureframe
+        if self.filtercounter > 0:  # show the current filter when switching
+            self.filtercounter -= 1
+            cv2.putText(frame, self.current_filter.__name__, (45, 375), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 5)
+            return frame
+        if self.framecount < 50:  # make a reference background out of 50 frames
+            self.pictureframe = frame
+            background.foregroundMask(frame)
+        fingercount = gestureDetection.detect_fingercount(frame)
+        if fingercount == 2:  # switch filter
+            self.current_filter = filtering.catalog[next(filtering.carousel)]
+            self.filtercounter = 50
+        if fingercount == 5:  # take a picture and process it
+            fgmask = background.foregroundMask(frame)
+            masked_frame = cv2.bitwise_and(frame, frame, mask=fgmask)
+            handled_frame = self.current_filter(masked_frame)
+            self.pictureframe = handled_frame
+            self.delaycounter = 100
+        self.framecount += 1
         return frame
-    if framecount < 50:  # make a reference background out of 50 frames
-        pictureframe = frame
-        create_background(frame)
-    fingercount = gestureDetection.detect_fingercount(frame)
-    if fingercount == 2:
-        current_filter = filtering.catalog[next(filtering.carousel)]
-        filtercounter = 50
-    if fingercount == 5:  # take a picture and process it
-        fgmask = background.foregroundMask(frame)
-        masked_frame = cv2.bitwise_and(frame, frame, mask=fgmask)
-        handled_frame = current_filter(masked_frame)
-        pictureframe = handled_frame
-        delaycounter = 100
-    framecount += 1
-    return frame
