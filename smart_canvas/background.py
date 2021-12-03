1da6702cf8ea37b70460aa4ff01c41aa0855f7a3
Jarkko Kotaniemi <jarkko.kotaniemi@vtt.fi>
2021-12-03 11:32:13 +0200

    ISSUE-51: Remove unnececcary variable and method

--- smart_canvas/background.py
+++ smart_canvas/background.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
 import mediapipe as mp
-mp_drawing = mp.solutions.drawing_utils
 mp_selfie_segmentation = mp.solutions.selfie_segmentation
 
 class ForegroundMask:
@@ -13,18 +12,6 @@ class ForegroundMask:
         self.selfie_segmentation = mp_selfie_segmentation.SelfieSegmentation(model_selection=1)
         self.bg_image = None
         self.output_image = None
-
-    def fill_holes(self, mask):
-        contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        len_contour = len (contours)
-        contour_list = []
-        for i in range(len_contour):
-            drawing = np.zeros_like(mask, np.uint8)  # create a black image
-            img_contour = cv2.drawContours(drawing, contours, i, (255, 255, 255), -1)
-            contour_list.append(img_contour)
-
-        out = sum(contour_list)
-        return out
 
     def remove_isolated_pixels(self, mask):
         kernel = np.ones((5, 5), np.uint8)
