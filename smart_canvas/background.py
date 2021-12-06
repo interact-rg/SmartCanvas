cd7bf82871adb9cc2027d839e2afa64870cd8ed5
Jarkko Kotaniemi <jarkko.kotaniemi@vtt.fi>
2021-12-06 15:13:18 +0200

    ISSUE-34: Change background after filtering

--- smart_canvas/background.py
+++ smart_canvas/background.py
@@ -10,8 +10,11 @@ class ForegroundMask:
 
     def __init__(self):
         self.selfie_segmentation = mp_selfie_segmentation.SelfieSegmentation(model_selection=1)
-        self.bg_image = None
+        self.bg_image = cv2.imread('smart_canvas/backgrounds/painterly_forest.jpg')
+        dim = (1280,720)
+        self.bg_image = cv2.resize(self.bg_image, dim, interpolation = cv2.INTER_AREA)
         self.output_image = None
+        self.mask = None
 
     def remove_isolated_pixels(self, mask):
         kernel = np.ones((5, 5), np.uint8)
@@ -29,9 +32,14 @@ class ForegroundMask:
         frame.flags.writeable = True
         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 
-        results.segmentation_mask = self.remove_isolated_pixels(results.segmentation_mask)
+        self.mask = self.remove_isolated_pixels(results.segmentation_mask)
 
-        condition = np.stack((results.segmentation_mask,) * 3, axis=-1) > 0.1
-        self.bg_image = np.zeros(frame.shape, dtype=np.uint8)
+        condition = np.stack((self.mask,) * 3, axis=-1) > 0.1
+        emptyBg = np.zeros(frame.shape, dtype=np.uint8)
+        self.output_image = np.where(condition, frame, emptyBg)
+        return self.output_image
+
+    def changeBackground(self, frame):
+        condition = np.stack((self.mask,) * 3, axis=-1) > 0.1
         self.output_image = np.where(condition, frame, self.bg_image)
         return self.output_image
