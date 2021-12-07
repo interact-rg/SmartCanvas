8e55ea184226ccff5b166974ed3d2dfdfd9998c0
Vili Tiinanen <vili.tiinanen@live.fi>
2021-12-07 11:52:37 +0200

    ISSUE-9: Test filters

---
+++ tests/filter_test.py
@@ -0,0 +1,20 @@
+""" filter_test.py """
+
+import cv2
+import pytest
+
+from smart_canvas.filters.carousel import FilterCarousel
+
+
+class TestFilters(object):
+    def test_filter_with_image(self):
+        filters = FilterCarousel()
+        frame = cv2.imread(f"tests/test_assets/small_image/image.png")
+        w, h, c = frame.shape
+        for i in range(0, 5):
+            filtered_frame = filters.current_filter(frame)
+            p_w, p_h, p_c = filtered_frame.shape
+            assert w == p_w
+            assert h == p_h
+            assert c == p_c
+            filters.next_filter()
