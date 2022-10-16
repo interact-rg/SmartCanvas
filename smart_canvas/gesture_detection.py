0d6841b8711143515f6985eec80014c2f16d6fe6
jouniwho <55877078+jouniwho@users.noreply.github.com>
2022-10-16 12:20:44 +0300

    added back positions and labels

--- smart_canvas/gesture_detection.py
+++ smart_canvas/gesture_detection.py
@@ -19,17 +19,26 @@ class HandDetect:
             min_tracking_confidence=0.8
         )
 
+    def findHandLandMarks(self, hand_landmark, label):
+
+        # label gives if hand is left or right
+        # account for inversion in webcams
+        if label == "Left":
+            label = "Right"
+        elif label == "Right":
+            label = "Left"
+
+        landMarkList = []
+
+        # Fill list with x and y positions of each landmark
+        for landmarks in hand_landmark.landmark:
+            landMarkList.append([landmarks.x, landmarks.y, label])
+
+        return landMarkList
+
     def count_fingers(self, frame):
         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         results = self.hands.process(image)
-        # hand landmark positions (x and y)
-        handLandmarks = []
-
-        # Draw the hand annotations on the image.
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-        # Initially set finger count to 0 for each cap
         fingerCount = 0
 
         if results.multi_hand_landmarks:
@@ -38,11 +47,10 @@ class HandDetect:
                 # Get hand index to check label (left or right)
                 handIndex = results.multi_hand_landmarks.index(hand_landmarks)
                 handLabel = results.multi_handedness[handIndex].classification[0].label
+                # hand landmark positions (x, y, label)
+                handLandmarks = self.findHandLandMarks(hand_landmarks, handLabel)
 
-                # Fill list with x and y positions of each landmark
-                for landmarks in hand_landmarks.landmark:
-                    handLandmarks.append([landmarks.x, landmarks.y])
-
+                #For thumbs we check the thumb TIP is over the thumb IP x
                 if handLabel == "Left" and handLandmarks[4][0] > handLandmarks[3][0]:   # Left thumb
                     fingerCount = fingerCount+1
                 elif handLabel == "Right" and handLandmarks[4][0] < handLandmarks[3][0]:    # Right thumb
@@ -58,6 +66,5 @@ class HandDetect:
                     fingerCount = fingerCount+1
                 if handLandmarks[20][1] < handLandmarks[18][1]:     #Pinky
                     fingerCount = fingerCount+1
-            # Display finger count
-            cv2.putText(frame, str(fingerCount), (50, 450), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 0), 10)
+            
         return fingerCount
