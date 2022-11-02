1a78b489c1dacc420ed51fa85d5b6941ec7d85a8
antilaanssi <anssi.antila@hotmail.com>
2022-11-02 12:03:04 +0200

    try to fix database tests

--- tests/database_test.py
+++ tests/database_test.py
@@ -5,26 +5,25 @@ from unittest.mock import MagicMock
 import os
 import sqlite3
 
-
 def test_get_mock():
-    session = MagicMock()
+    session = MagicMock() # 1
     executor = MagicMock()
     session.execute = executor
-    cache = CacheService(session)
-    cache.get_status(1)
-    executor.assert_called_once_with('SELECT image_id FROM images WHERE image_id=?', (1))
+    cache = CacheService(session) # 2
+    cache.get_status('+3155512345')
+    executor.assert_called_once_with('SELECT existing FROM numbers WHERE number=?', ('+3155512345',)) # 3
 
-@pytest.mark.usefixtures("setup_db")
-def test_get(cache):
-    existing = cache.get_status(1)
+def test_get(session): # 1
+    cache = CacheService(session) # 2
+    existing = cache.get_status('+3155512345') # 3
     assert existing
 
-'''def test_get_unknown(session):
+def test_get_unknown(session):
     cache = CacheService(session)
-    assert cache.get_status(1) is None
+    assert cache.get_status('+315554444') is None
 
 def test_save(session):
-    number = 1
+    number = '+3155512346'
     cache = CacheService(session)
     cache.save_status(number, True)
     existing = cache.get_status(number)
@@ -32,63 +31,53 @@ def test_save(session):
 
 def test_report(session):
     cache = CacheService(session)
-    cache.save_status(1, True)
-    cache.save_status(1, False)
-    cache.save_status(1, False)
+    cache.save_status('+3155512346', True)
+    cache.save_status('+3155512347', False)
+    cache.save_status('+3155512348', False)
     ratio = cache.generate_report()
-    assert ratio == 0.5'''
+    assert ratio == 0.5
 
 
-class CacheService:
-    def __init__(self, session):
-        self.session = session
-
-    def get_status(self, number):
-        print("GETTING STATUS!!!!!!!!!!!!!!!!!!")
-        self.session.execute('SELECT image_id FROM images WHERE image_id=?', (number))
-        return self.session.fetchone()
-
-    def save_status(self, number, existing):
-        self.session.execute('INSERT INTO images image_id VALUES (?)', (number))
-        self.session.connection.commit()
-
-    def generate_report(self):
-        self.session.execute('SELECT * FROM images')
-        count = self.session.fetchone()
-        self.session.execute('SELECT * FROM images WHERE image_id=1')
-        count_existing = self.session.fetchone()
-        return count_existing[0]/count[0]
-
-
-@pytest.fixture()
-def session():
+@pytest.fixture
+def session(): # 1
     connection = sqlite3.connect(':memory:')
     db_session = connection.cursor()
     yield db_session
     connection.close()
 
 
-@pytest.fixture()
-def setup_db(session):
-    session.execute('''CREATE TABLE images (
-    image_id int,
-    image mediumblob,
-    date_added date
-    );''')
-    print("DB HAS BEEN SET UP!!!!!!!!!")
-    image_id = 1
-    script_dir = os.path.dirname(__file__)
-    rel_path = r"assets\logo.png"
-    image = os.path.join(script_dir, rel_path)
-    print(f"image path: {image}")
-    date_added = datetime.datetime.now()
-    sql_insert_blob_query = """ INSERT INTO images
-                        (image_id, image, date_added) VALUES (%s,%s,%s)"""
-
-    session.execute(sql_insert_blob_query)
-    
+@pytest.fixture
+def setup_db(session): # 2
+    session.execute('''CREATE TABLE numbers
+                          (number text, existing boolean)''')
+    session.execute('INSERT INTO numbers VALUES ("+3155512345", 1)')
     session.connection.commit()
 
-@pytest.fixture()
-def cache(session): 
+
+@pytest.fixture
+def cache(session): # 1
     return CacheService(session)
+
+@pytest.mark.usefixtures("setup_db")
+def test_get(cache): # 2
+    existing = cache.get_status('+3155512345')
+    assert existing
+
+class CacheService:
+    def __init__(self, session): # 1
+        self.session = session # 2
+
+    def get_status(self, number):
+        self.session.execute('SELECT existing FROM numbers WHERE number=?', (number,))
+        return self.session.fetchone()
+
+    def save_status(self, number, existing):
+        self.session.execute('INSERT INTO numbers VALUES (?, ?)', (number, existing))
+        self.session.connection.commit()
+
+    def generate_report(self):
+        self.session.execute('SELECT COUNT(*) FROM numbers')
+        count = self.session.fetchone()
+        self.session.execute('SELECT COUNT(*) FROM numbers WHERE existing=1')
+        count_existing = self.session.fetchone()
+        return count_existing[0]/count[0]
