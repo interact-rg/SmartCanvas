83ad507ceb865ee6963062df0b28da09f32fa921
antilaanssi <anssi.antila@hotmail.com>
2022-11-02 09:37:42 +0200

    try to fix database tests

--- tests/database_test.py
+++ tests/database_test.py
@@ -6,7 +6,7 @@ from unittest.mock import MagicMock
 
 
 class CacheService:
-    def __init__(session):
+    def __init__(self, session):
         self.session = session
 
     def get_status(self, number):
@@ -25,56 +25,56 @@ class CacheService:
         return count_existing[0]/count[0]
 
 
-@pytest.fixture
-def session():
-    connection = mysql.connector.connect(':memory:')
-    db_session = connection.cursor()
-    yield db_session
-    connection.close()
+    @pytest.fixture
+    def session():
+        connection = mysql.connector.connect(':memory:')
+        db_session = connection.cursor()
+        yield db_session
+        connection.close()
 
 
-@pytest.fixture
-def setup_db(session):
-    session.execute('''CREATE TABLE numbers
-                          (number text, existing boolean)''')
-    session.execute('INSERT INTO numbers VALUES ("+3155512345", 1)')
-    session.connection.commit()
+    @pytest.fixture
+    def setup_db(session):
+        session.execute('''CREATE TABLE numbers
+                            (number text, existing boolean)''')
+        session.execute('INSERT INTO numbers VALUES ("+3155512345", 1)')
+        session.connection.commit()
 
-def test_get_mock():
-    session = MagicMock()
-    executor = MagicMock()
-    session.execute = executor
-    cache = CacheService(session)
-    cache.get_status('+3155512345')
-    executor.assert_called_once_with('SELECT existing FROM numbers WHERE number=?', ('+3155512345',)) # 3
-
-
+    def test_get_mock():
+        session = MagicMock()
+        executor = MagicMock()
+        session.execute = executor
+        cache = CacheService(session)
+        cache.get_status('+3155512345')
+        executor.assert_called_once_with('SELECT existing FROM numbers WHERE number=?', ('+3155512345',)) # 3
 
 
-@pytest.fixture
-def cache(session): 
-    return CacheService(session)
 
-@pytest.mark.usefixtures("setup_db")
-def test_get(cache):
-    existing = cache.get_status('+3155512345')
-    assert existing
 
-def test_get_unknown(session):
-    cache = CacheService(session)
-    assert cache.get_status('+315554444') is None
+    @pytest.fixture
+    def cache(session): 
+        return CacheService(session)
 
-def test_save(session):
-    number = '+3155512346'
-    cache = CacheService(session)
-    cache.save_status(number, True)
-    existing = cache.get_status(number)
-    assert existing
+    @pytest.mark.usefixtures("setup_db")
+    def test_get(cache):
+        existing = cache.get_status('+3155512345')
+        assert existing
 
-def test_report(session):
-    cache = CacheService(session)
-    cache.save_status('+3155512346', True)
-    cache.save_status('+3155512347', False)
-    cache.save_status('+3155512348', False)
-    ratio = cache.generate_report()
-    assert ratio == 0.5
+    def test_get_unknown(session):
+        cache = CacheService(session)
+        assert cache.get_status('+315554444') is None
+
+    def test_save(session):
+        number = '+3155512346'
+        cache = CacheService(session)
+        cache.save_status(number, True)
+        existing = cache.get_status(number)
+        assert existing
+
+    def test_report(session):
+        cache = CacheService(session)
+        cache.save_status('+3155512346', True)
+        cache.save_status('+3155512347', False)
+        cache.save_status('+3155512348', False)
+        ratio = cache.generate_report()
+        assert ratio == 0.5
