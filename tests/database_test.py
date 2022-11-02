75577ac35f3cb400a62441f75ecd85b5f8377ad1
antilaanssi <anssi.antila@hotmail.com>
2022-11-02 12:44:43 +0200

    try to fix database tests

--- tests/database_test.py
+++ tests/database_test.py
@@ -16,10 +16,17 @@ def session(): # 1
 
 @pytest.fixture
 def setup_db(session): # 2
-    session.execute('''CREATE TABLE numbers
-                          (number text, existing boolean)''')
-    session.execute('INSERT INTO numbers VALUES ("+3155512345", 1)')
-    session.execute('Select * from "numbers"')
+    session.execute('''CREATE TABLE images (
+    image_id int,
+    image mediumblob,
+    date_added date
+    );''')
+    image_id = 1
+    
+    script_dir = os.path.dirname(__file__)
+    rel_path = r"test_assets/finger_pictures"
+    image = os.path.join(script_dir, rel_path)
+    session.execute('INSERT INTO images (image_id, image, date_added) VALUES (%s,%s,%s)')
     session.connection.commit()
 
 @pytest.mark.usefixtures("setup_db")
@@ -28,39 +35,27 @@ def test_get_mock():
     executor = MagicMock()
     session.execute = executor
     cache = CacheService(session) # 2
-    cache.get_status('+3155512345')
-    executor.assert_called_once_with('SELECT existing FROM numbers WHERE number=?', ('+3155512345',)) # 3
+    cache.get_status('1')
+    executor.assert_called_once_with('SELECT image_id FROM numbers WHERE image_id=?', ('1',)) # 3
 
 @pytest.mark.usefixtures("setup_db")
 def test_get(session): # 1
-    print("!!!!!!!!!!!!!!!!TESTING GET METHOD!!!!!!!!!!!")
     cache = CacheService(session) # 2
-    existing = cache.get_status('+3155512345') # 3
+    existing = cache.get_status('1') # 3
     assert existing
 
 @pytest.mark.usefixtures("setup_db")
 def test_get_unknown(session):
     cache = CacheService(session)
-    assert cache.get_status('+315554444') is None
+    assert cache.get_status('15') is None
 
 @pytest.mark.usefixtures("setup_db")
 def test_save(session):
-    number = '+3155512346'
+    number = '2'
     cache = CacheService(session)
     cache.save_status(number, True)
     existing = cache.get_status(number)
     assert existing
-
-@pytest.mark.usefixtures("setup_db")
-def test_report(session):
-    cache = CacheService(session)
-    cache.save_status('+3155512346', True)
-    cache.save_status('+3155512347', False)
-    cache.save_status('+3155512348', False)
-    ratio = cache.generate_report()
-    assert ratio == 0.5
-
-
 
 @pytest.fixture
 def cache(session): # 1
@@ -68,7 +63,7 @@ def cache(session): # 1
 
 @pytest.mark.usefixtures("setup_db")
 def test_get(cache): # 2
-    existing = cache.get_status('+3155512345')
+    existing = cache.get_status('1')
     assert existing
 
 class CacheService:
@@ -80,12 +75,10 @@ class CacheService:
         return self.session.fetchone()
 
     def save_status(self, number, existing):
-        self.session.execute('INSERT INTO numbers VALUES (?, ?)', (number, existing))
+        image_id = number
+    
+        script_dir = os.path.dirname(__file__)
+        rel_path = r"test_assets/finger_pictures"
+        image = os.path.join(script_dir, rel_path)
+        self.session.execute('INSERT INTO images (image_id, image, date_added) VALUES (%s,%s,%s)')
         self.session.connection.commit()
-
-    def generate_report(self):
-        self.session.execute('SELECT COUNT(*) FROM numbers')
-        count = self.session.fetchone()
-        self.session.execute('SELECT COUNT(*) FROM numbers WHERE existing=1')
-        count_existing = self.session.fetchone()
-        return count_existing[0]/count[0]
