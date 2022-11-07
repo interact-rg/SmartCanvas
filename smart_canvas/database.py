608f53c8adf256dd6978e6c9d526d37f6705b44c
antilaanssi <anssi.antila@hotmail.com>
2022-11-07 09:36:43 +0200

    change database to sqlite from mysql

--- smart_canvas/database.py
+++ smart_canvas/database.py
@@ -1,6 +1,5 @@
 """ database.py """
-
-import mysql.connector
+import sqlite3
 import datetime
 import typing
 from PIL import Image as im
@@ -8,6 +7,39 @@ import os
 
 
 class Database:
+
+    def create_database(self):
+        print("Creating database...")
+        connection = sqlite3.connect('database.db')
+        with open('schema.sql') as f:
+            connection.executescript(f.read())
+        cursor = connection.cursor()
+
+        #check if database exists
+        cursor.execute("SHOW DATABASES")
+        for x in cursor:
+            #error if database doesn't exist
+            print(x)
+        cursor.execute("CREATE DATABASE smartcanvas")
+
+
+        cursor.execute('''CREATE TABLE images (
+        image_id int,
+        image mediumblob,
+        date_added date
+        );''')
+
+        sql_select_query = """SELECT * from images"""
+        cursor.execute(sql_select_query)
+        myresult = cursor.fetchall()
+
+
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("SQLite connection is closed")
+
+
     def convert_image_to_binary(self, filename) -> bytes:
         # Convert digital data to binary format
         with open(filename, "rb") as file:
@@ -24,49 +56,41 @@ class Database:
         )  # remove this later, no need to save locally.
         image_id = 1
         date_added = datetime.datetime.now()
-        # maybe add information of database from configuration file for safety.............
-        try:
-            connection = mysql.connector.connect(
-                host="localhost",
-                database="smartcanvas",
-                user="canvas",
-                password="SmartCanvasV",
-            )
+        connection = sqlite3.connect('database.db')
 
-            cursor = connection.cursor()
+        cursor = connection.cursor()
 
-            sql_select_query = """SELECT * from images"""
-            cursor.execute(sql_select_query)
-            myresult = cursor.fetchall()
-            # check if id exists in database:
-            for row in myresult:
-                if row[0] == image_id:
-                    image_id += 1
+        sql_select_query = """SELECT * from images"""
+        cursor.execute(sql_select_query)
+        myresult = cursor.fetchall()
+        # check if id exists in database:
+        for row in myresult:
+            if row[0] == image_id:
+                image_id += 1
 
-            sql_insert_blob_query = """ INSERT INTO images
-                            (image_id, image, date_added) VALUES (%s,%s,%s)"""
+        sql_insert_blob_query = """ INSERT INTO images
+                                (image_id, image, date_added) VALUES (?, ?, ?)"""
 
-            image = self.convert_image_to_binary(r"assets\picwithcanvas.png")
+        image = self.convert_image_to_binary(r"assets\picwithcanvas.png")
 
-            # Convert data into tuple format
-            print(f"image_id is: {image_id}")
-            insert_blob_tuple = (image_id, image, date_added)
-            result = cursor.execute(sql_insert_blob_query, insert_blob_tuple)
-            connection.commit()
-            print("Image and file inserted successfully as a BLOB into images table")
-            if os.path.exists(r"assets\picwithcanvas.png"):
-                os.remove(r"assets\picwithcanvas.png")
-            else:
-                print("The file does not exist")
+        image = r"test_assets/finger_pictures"
+        # Convert data into tuple format
+        data_tuple = (image_id, image, date_added)
+        cursor.execute(sql_insert_blob_query, data_tuple)
 
-        except mysql.connector.Error as error:
-            print("Failed inserting BLOB data into MySQL table {}".format(error))
+        connection.commit()
+        print("Image inserted successfully as a BLOB into images table")
+        #delete image that was sent to database<
+        if os.path.exists(r"assets\picwithcanvas.png"): 
+            os.remove(r"assets\picwithcanvas.png")
+        else:
+            print("The file does not exist")
 
-        finally:
-            if connection.is_connected():
-                cursor.close()
-                connection.close()
-                print("MySQL connection is closed")
+
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("SQLITE connection is closed")
 
     #  function to convert binary to image
     def convert_binary_to_image(self, data: bytes, file_name: str):
@@ -77,61 +101,43 @@ class Database:
 
     # download image with given id
     def download(self, image_id: int):
-        try:
-            # establish connection
-            connection = mysql.connector.connect(
-                host="localhost",
-                database="smartcanvas",
-                user="canvas",
-                password="SmartCanvasV",
-            )
-            cursor = connection.cursor()
-            # getting data by id value
-            query = """ SELECT * from images where image_id = %s """
+        # establish connection
+        connection = sqlite3.connect('database.db')
+        cursor = connection.cursor()
+        # getting data by id value
+        query = """ SELECT * from images where image_id = %s """
 
-            cursor.execute(query, (image_id,))
-            result = cursor.fetchall()
-            for row in result:
-                print("image Id = ", row[0])
-                image = row[1]
-                print("date  = ", row[2])
-                # Pass path with filename where we want to save our file
-            self.convert_binary_to_image(image, r"assets\downloadedimage.png")
-            print("Successfully Retrieved Values from database")
+        cursor.execute(query, (image_id,))
+        result = cursor.fetchall()
+        for row in result:
+            print("image Id = ", row[0])
+            image = row[1]
+            print("date  = ", row[2])
+            # Pass path with filename where we want to save our file
+        self.convert_binary_to_image(image, r"assets\downloadedimage.png")
+        print("Successfully Retrieved Values from database")
 
-        except mysql.connector.Error as error:
-            print(format(error))
-
-        finally:
-            if connection.is_connected():
-                cursor.close()
-                connection.close()
-                print("MySQL connection is closed")
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("SQLite connection is closed")
 
     def delete(self):
         # function to check if database has items older than a week.
-        print("accessed through thread, lets try")
-        try:
-            # establish connection
-            connection = mysql.connector.connect(
-                host="localhost",
-                database="smartcanvas",
-                user="canvas",
-                password="SmartCanvasV",
-            )
-            cursor = connection.cursor()
-            
-            sql_delete_query = f""" Delete from images where date_added < now() - interval 1 week;"""
+        # establish connection
+        print("Deleting values older than week ")
+        connection = sqlite3.connect('database.db')
+        cursor = connection.cursor()
+        sql_delete_query = f""" Delete from images where date_added < now() - interval 1 week;"""
 
 
-            cursor.execute(sql_delete_query)
-            result = cursor.fetchall()
+        cursor.execute(sql_delete_query)
+        result = cursor.fetchall()
+        #check if something was actually deleted:
+        for x in result:
+            print(x)
 
-            print("Successfully deleted Values from database")
-
-        except mysql.connector.Error as error:
-            print(format(error))
-        pass
+        print("Successfully deleted Values from database")
 
 
 base = Database()
