8dbfe50e219b46c2dc045f2746f4a3b13917d314
Juuso Herajärvi <juusoherajarvi@hotmail.com>
2023-10-26 16:06:35 +0300

    New backgrounds

--- smart_canvas/background.py
+++ smart_canvas/background.py
@@ -19,9 +19,9 @@ class ForegroundMask:
     def switchBackground(self, current_filter):
         filter_images_lib = {
             'painterly': 'painterly_bg.jpg',
-            'watercolor': 'pointillism_bg.jpg',
+            'watercolor': 'watercolor_bg.jpg',
             'oil painting': 'oil_painting_bg.jpg',
-            'mosaic': 'pointillism_bg.jpg',
+            'mosaic': 'mosaic_bg.jpg',
             'grayscale cartoon': 'gs_cartoon_bg_2.jpeg',
             'anime style': 'anime_bg.jpg',
             'pointillism': 'pointillism_bg.jpg'
