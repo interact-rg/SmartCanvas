bac680af0ae5aaf7b89a8c45b604670638982005
Romi Jokela <jokelaromi@gmail.com>
2025-01-31 19:38:32 +0200

    Comment out firefox block in flask for now

--- web/main/routes.py
+++ web/main/routes.py
@@ -30,16 +30,16 @@ def verify_token(token):
 @main.route('/')
 def index():
     """ Home page """
-    if "Firefox" in request.headers.get('User-Agent'):
-        return render_template('us_browser.html')
+    # if "Firefox" in request.headers.get('User-Agent'):
+    #     return render_template('us_browser.html')
     return render_template('index.html')
 
 
 @main.route('/fullscreen')
 def fs_sym():
     """ Fullscreen """
-    if "Firefox" in request.headers.get('User-Agent'):
-        return render_template('us_browser.html')
+    # if "Firefox" in request.headers.get('User-Agent'):
+    #     return render_template('us_browser.html')
     return render_template('fullscreen.html')
 
 
