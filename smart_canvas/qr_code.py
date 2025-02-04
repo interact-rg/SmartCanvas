696929d059a0f09fad3f0a5f8422fe9a7884f163
Romi Jokela <jokelaromi@gmail.com>
2025-02-05 00:42:06 +0200

    adjust qr_code.py to match breaking changes to qrcode

--- smart_canvas/qr_code.py
+++ smart_canvas/qr_code.py
@@ -1,11 +1,13 @@
 """ cr_code.py """
 import qrcode
+from qrcode import constants as qrcode_constants
 from PIL import Image
+from PIL.Image import Resampling
 import cv2
 import numpy
 
 
-def create_qr_code(url):
+def create_qr_code(url: str):
     """
     Creates QR code for url and outputs it as cv2 image
     """
@@ -17,23 +19,23 @@ def create_qr_code(url):
     logo_width, logo_height = logo.size
     w_percent = (basewidth / float(logo_width))
     h_size = int((float(logo_height) * float(w_percent)))
-    resized_logo = logo.resize((basewidth, h_size), Image.ANTIALIAS)
+    resized_logo = logo.resize((basewidth, h_size), Resampling.BICUBIC)
     r_logo_width, r_logo_height = resized_logo.size
 
     QRcode = qrcode.QRCode(
-        error_correction=qrcode.constants.ERROR_CORRECT_H,
+        error_correction=qrcode_constants.ERROR_CORRECT_H,
     )
     QRcode.add_data(url)
     QRcode.make()
     QRimg = QRcode.make_image(
         fill_color='Black',
         back_color='White'
-    ).convert('RGB')
+    ).get_image()
     QR_width, QR_height = QRimg.size
     pos = (
         (QR_width - r_logo_width) // 2,
         (QR_height - r_logo_height) // 2
     )
     QRimg.paste(resized_logo, pos)
-    cv2_QR_image = cv2.cvtColor(numpy.array(QRimg), cv2.COLOR_RGB2BGR)
+    cv2_QR_image = cv2.cvtColor(numpy.array(QRimg.convert('RGB')), cv2.COLOR_RGB2BGR)
     return cv2_QR_image
