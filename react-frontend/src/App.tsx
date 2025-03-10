91a7740099e9c88d4c199e0855cfe4b8b6c2e480
naanatin <naanatin@gmail.com>
2025-03-10 16:06:29 +0200

    react-ts app for frontend

---
+++ react-frontend/src/App.tsx
@@ -0,0 +1,18 @@
+//import { useState } from 'react'
+import './styles/style.css'
+import TopNav from './components/TopNav'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className="tos-bg">
+      <TopNav />
+    </div>
+      
+    </>
+  )
+}
+
+export default App
