7b2fbb72e320fa791215e9a9f3af221a81f2d5d7
Pauli Klemettila <pauli.klemettila@gmail.com>
2025-03-26 15:15:30 +0200

    changed face and gesture recognition to video mode

--- smart_canvas/face_detection.py
+++ smart_canvas/face_detection.py
@@ -9,18 +9,21 @@ class FaceDetection:
 
     def __init__(self):
 
-        
-        base_options = python.BaseOptions(model_asset_path='models/blaze_face_short_range.tflite')
-        options = vision.FaceDetectorOptions(base_options=base_options)
-        self.detector = vision.FaceDetector.create_from_options(options)
+
+        self.options = vision.FaceDetectorOptions(
+            base_options=python.BaseOptions(model_asset_path='models/blaze_face_short_range.tflite'),
+            running_mode=vision.RunningMode.VIDEO,  
+         )
+        self.detector = vision.FaceDetector.create_from_options(self.options)
+
 
         if self.detector is None:
-            raise RuntimeError("Failed to load face detection model from" + base_options.model_asset_path)
-        else: print("Face detection model loaded from" + base_options.model_asset_path)
-
+            raise RuntimeError("Failed to load face detection model from" + vision.FaceDetectorOptions.base_options.model_asset_path)
+        
         self.stable_time = None
         self.unstable_start_time = None
 
+        self.last_timestamp = time.time() * 1000
 
 
     def detect_face(self, frame: MatLike) -> tuple[bool, float]:
@@ -30,7 +33,13 @@ class FaceDetection:
 
         #convert to mediapipe image
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame_rgb)
-        detection_result = self.detector.detect(mp_image)
+        
+        timestamp = int(time.time() * 1000)
+        if timestamp <= self.last_timestamp:
+            timestamp = self.last_timestamp + 1
+            self.last_timestamp = timestamp
+
+        detection_result = self.detector.detect_for_video(mp_image, timestamp)
 
         face_present = bool(detection_result and detection_result.detections)
         current_time = time.time()
