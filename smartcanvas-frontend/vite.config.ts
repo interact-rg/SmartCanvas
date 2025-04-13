1cf8705072f07d3ff874e6cdc835063b03d3345f
Pauli Klemettila <pauli.klemettila@gmail.com>
2025-04-13 22:57:54 +0300

    added csc pouta to allowed hosts in vite config

--- smartcanvas-frontend/vite.config.ts
+++ smartcanvas-frontend/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: ['vm1029.kaj.pouta.csc.fi']
+  }
 })
