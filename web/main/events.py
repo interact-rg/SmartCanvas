2024470fc0d2face92c415689b83f160e099bd41
Romi Jokela <jokelaromi@gmail.com>
2025-04-14 15:47:50 +0300

    include protocol in hostname

--- web/main/events.py
+++ web/main/events.py
@@ -36,11 +36,11 @@ def connect_web():
     image_stores.update({sid: ImageStore()})
     try: 
         if request.values['version'] == 'alternate':
-            core_threads.update({sid: CanvasCoreAlternate(q_consumer=core_queues[sid], img_store=image_stores[sid], sid=sid, hostname=request.host).start()})
+            core_threads.update({sid: CanvasCoreAlternate(q_consumer=core_queues[sid], img_store=image_stores[sid], sid=sid, hostname=request.host_url).start()})
         else:
-            core_threads.update({sid: CanvasCore(q_consumer=core_queues[sid], img_store=image_stores[sid], sid=sid, hostname=request.host).start()})
+            core_threads.update({sid: CanvasCore(q_consumer=core_queues[sid], img_store=image_stores[sid], sid=sid, hostname=request.host_url).start()})
     except Exception as e:
-        core_threads.update({sid: CanvasCore(q_consumer=core_queues[sid], img_store=image_stores[sid], sid=sid, hostname=request.host).start()})
+        core_threads.update({sid: CanvasCore(q_consumer=core_queues[sid], img_store=image_stores[sid], sid=sid, hostname=request.host_url).start()})
     socketio.emit('available_filters', core_threads[sid].get_available_filters(), to=sid)
 
 
