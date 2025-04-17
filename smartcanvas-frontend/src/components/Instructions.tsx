182877642b1b572bf7a2dac2174dbdfc51fe06f2
Petercode12 <phamphuoc19032001@gmail.com>
2025-04-17 11:14:10 +0300

    Update hand instructions

--- smartcanvas-frontend/src/components/Instructions.tsx
+++ smartcanvas-frontend/src/components/Instructions.tsx
@@ -1,10 +1,10 @@
 /**
  * Component to show different instructions based on the state of the application
  */
-import React, {useState, useEffect } from "react";
-import waving_hand from "../assets/five_fingers.png"
-import "../styles/hand.css"
-import swiping_hand from "../assets/swiping_hand.png"
+import React, { useState, useEffect } from "react";
+import waving_hand from "../assets/five_fingers.png";
+import "../styles/hand.css";
+import swiping_hand from "../assets/swiping_hand.png";
 
 interface InstructionsProps {
   state: { [key: string]: any };
@@ -12,7 +12,11 @@ interface InstructionsProps {
   filter: string;
 }
 
-const Instructions: React.FC<InstructionsProps> = ({ state, countdown = 4, filter }) => {
+const Instructions: React.FC<InstructionsProps> = ({
+  state,
+  countdown = 4,
+  filter,
+}) => {
   const [randomColumn, setRandomColumn] = useState<number>(1); // Random column (1, 2, or 3)
   const [waveIsVisible, setWaveIsVisible] = useState<boolean>(true); // Toggle visibility of the waving hand
   const [swipeIsVisible, setSwipeVisible] = useState<boolean>(true); //instructions (swiping_hand) should be invisible for n seconds after filter is changed (user has learned how to switch filters, so instructions don't need to be visible)
@@ -33,7 +37,7 @@ const Instructions: React.FC<InstructionsProps> = ({ state, countdown = 4, filte
     return () => clearInterval(interval); // Cleanup interval on unmount
   }, [waveIsVisible]);
 
-  useEffect (() => {
+  useEffect(() => {
     if (state.Countdown) {
       countdown = 4; // Reset countdown to 4 seconds when countdown state is active
     }
@@ -45,7 +49,7 @@ const Instructions: React.FC<InstructionsProps> = ({ state, countdown = 4, filte
       const interval = setInterval(() => {
         setSwipeVisible((prev) => !prev); // Toggle visibility
       }, 5000);
-    
+
       return () => clearInterval(interval); // Cleanup interval on unmount
     }
   }, [state, swipeUsed]);
@@ -53,7 +57,7 @@ const Instructions: React.FC<InstructionsProps> = ({ state, countdown = 4, filte
   useEffect(() => {
     // Timer to toggle visibility of hold hand symbol every 6 seconds
     const interval = setInterval(() => {
-      setHoldHandVisible((prev) => !prev); // Toggle visibility  
+      setHoldHandVisible((prev) => !prev); // Toggle visibility
     }, 6000);
 
     return () => clearInterval(interval); // Cleanup interval on unmount
@@ -66,7 +70,7 @@ const Instructions: React.FC<InstructionsProps> = ({ state, countdown = 4, filte
       setSwipeUsed(true); // Set the flag to indicate that swiping hand has been used
     }
   }, [filter]);
-  
+
   const renderInstructions = () => {
     for (const [key, value] of Object.entries(state)) {
       if (value) {
@@ -81,13 +85,31 @@ const Instructions: React.FC<InstructionsProps> = ({ state, countdown = 4, filte
             return (
               <div className="top-row">
                 <div className="column" id="column-1">
-                  {waveIsVisible && randomColumn === 1 && <img src={waving_hand} id="hand" style={{maxWidth:'20%', marginLeft:'30%'}} />}
+                  {waveIsVisible && randomColumn === 1 && (
+                    <img
+                      src={waving_hand}
+                      id="hand"
+                      style={{ maxWidth: "20%", marginLeft: "30%" }}
+                    />
+                  )}
                 </div>
                 <div className="column" id="column-2">
-                  {waveIsVisible && randomColumn === 2 && <img src={waving_hand} id="hand" style={{maxWidth:'20%', marginLeft:'30%'}} />}
+                  {waveIsVisible && randomColumn === 2 && (
+                    <img
+                      src={waving_hand}
+                      id="hand"
+                      style={{ maxWidth: "20%", marginLeft: "30%" }}
+                    />
+                  )}
                 </div>
                 <div className="column" id="column-3">
-                  {waveIsVisible && randomColumn === 3 && <img src={waving_hand} id="hand" style={{maxWidth:'20%', marginLeft:'30%'}} />}
+                  {waveIsVisible && randomColumn === 3 && (
+                    <img
+                      src={waving_hand}
+                      id="hand"
+                      style={{ maxWidth: "20%", marginLeft: "30%" }}
+                    />
+                  )}
                 </div>
               </div>
             );
@@ -96,8 +118,24 @@ const Instructions: React.FC<InstructionsProps> = ({ state, countdown = 4, filte
               <div className="instructions">
                 <div className="top-row">
                   <div className="column" id="column-1"></div>
-                  <div className="column" id="column-2">{swipeIsVisible && <img src={swiping_hand} id="two_fingers_icon" style={{maxWidth: '20%'}}  />}</div>
-                  <div className="column" id="column-3">{holdHandVisible && <img src={waving_hand} id="hand_still" style={{maxWidth:'20%', marginLeft:'30%'}} />}</div>
+                  <div className="column" id="column-2">
+                    {swipeIsVisible && (
+                      <img
+                        src={swiping_hand}
+                        id="two_fingers_icon"
+                        style={{ maxWidth: "20%" }}
+                      />
+                    )}
+                  </div>
+                  <div className="column" id="column-3">
+                    {holdHandVisible && (
+                      <img
+                        src={waving_hand}
+                        id="hand_still"
+                        style={{ maxWidth: "20%", marginLeft: "30%" }}
+                      />
+                    )}
+                  </div>
                 </div>
                 <div className="bottom-row"></div>
               </div>
@@ -105,7 +143,7 @@ const Instructions: React.FC<InstructionsProps> = ({ state, countdown = 4, filte
           case "Countdown":
             return (
               <div className="full-container">
-                  <div className="big-text">{renderCountdown()}</div>
+                <div className="big-text">{renderCountdown()}</div>
               </div>
             );
 
@@ -132,16 +170,10 @@ const Instructions: React.FC<InstructionsProps> = ({ state, countdown = 4, filte
   //     setSwipeVisible((prev) => !prev);
   //     console.log("Swiping hand visible (should be true): " + swipeIsVisible)
   //   }, 5000);
-    
+
   // }
 
-  return (
-    <div className="instructions">
-      
-        {renderInstructions()}
-      
-    </div>
-  );
+  return <div className="instructions">{renderInstructions()}</div>;
 };
 
 export default Instructions;
