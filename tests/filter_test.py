f9847d933b03d207426971596b18551170a09723
Romi Jokela <jokelaromi@gmail.com>
2025-05-01 01:35:59 +0300

    dynamically generate test labels for all filters

--- tests/filter_test.py
+++ tests/filter_test.py
@@ -5,81 +5,73 @@ import pytest
 import numpy as np
 
 from smart_canvas.filters.carousel import FilterCarousel
+from smart_canvas.filters.base import Filter
+
+def build_filter_catalog():
+    carousel = FilterCarousel()
+    return [
+        pytest.param(filter, name, id=name) for name, filter in carousel.catalog.items()
+    ]
+    
 
 
-@pytest.fixture()
-def carousel():
-    yield FilterCarousel()
-
-
+@pytest.mark.parametrize("filter, name", build_filter_catalog())
 class TestFilters(object):
-    def test_filter_with_image(self, carousel: FilterCarousel):
+    def test_filter_with_image(self, filter: Filter, name: str):
         frame = cv2.imread(f"tests/test_assets/small_image/image.png")
         mask = cv2.imread(f"tests/test_assets/small_image/mask.png")
         w, h, c = frame.shape
-        for _ in carousel.catalog:
-            filtered_frame = carousel.current_filter.filter_frame(frame, mask)
-            p_w, p_h, p_c = filtered_frame.shape
-            assert w == p_w, f'wrong width in: {carousel.current_name}'
-            assert h == p_h, f'wrong height in: {carousel.current_name}'
-            assert c == p_c, f'wrong amount of color channels in: {carousel.current_name}'
-            carousel.next_filter()
+        filtered_frame = filter.filter_frame(frame, mask)
+        p_w, p_h, p_c = filtered_frame.shape
+        assert w == p_w, f'wrong width in: {name}'
+        assert h == p_h, f'wrong height in: {name}'
+        assert c == p_c, f'wrong amount of color channels in: {name}'
 
-    def test_filter_with_black_image(self, carousel):
+    def test_filter_with_black_image(self, filter: Filter, name: str):
         frame = np.zeros((50, 50, 3), np.uint8)
         mask = np.zeros((50, 50, 3), np.uint8)
         w, h, c = frame.shape
-        for _ in carousel.catalog:
-            filtered_frame = carousel.current_filter.filter_frame(frame, mask)
-            p_w, p_h, p_c = filtered_frame.shape
-            assert w == p_w, f'wrong width in: {carousel.current_name}'
-            assert h == p_h, f'wrong height in: {carousel.current_name}'
-            assert c == p_c, f'wrong amount of color channels in: {carousel.current_name}'
-            carousel.next_filter()
+        filtered_frame = filter.filter_frame(frame, mask)
+        p_w, p_h, p_c = filtered_frame.shape
+        assert w == p_w, f'wrong width in: {name}'
+        assert h == p_h, f'wrong height in: {name}'
+        assert c == p_c, f'wrong amount of color channels in: {name}'
 
-    def test_filter_with_white_image(self, carousel):
+    def test_filter_with_white_image(self, filter: Filter, name: str):
         frame = np.zeros((50, 50, 3), np.uint8)
         mask = np.zeros((50, 50, 3), np.uint8)
         frame[:] = 255
         mask[:] = 255
         w, h, c = frame.shape
-        for _ in carousel.catalog:
-            filtered_frame = carousel.current_filter.filter_frame(frame, mask)
-            p_w, p_h, p_c = filtered_frame.shape
-            assert w == p_w, f'wrong width in: {carousel.current_name}'
-            assert h == p_h, f'wrong height in: {carousel.current_name}'
-            assert c == p_c, f'wrong amount of color channels in: {carousel.current_name}'
-            carousel.next_filter()
+        filtered_frame = filter.filter_frame(frame, mask)
+        p_w, p_h, p_c = filtered_frame.shape
+        assert w == p_w, f'wrong width in: {name}'
+        assert h == p_h, f'wrong height in: {name}'
+        assert c == p_c, f'wrong amount of color channels in: {name}'
 
     # TODO: This test breaks alot of filters if smaller size image
-    def test_filter_with_min_imagesize(self, carousel):
+    def test_filter_with_min_imagesize(self, filter: Filter, name: str):
         frame = (np.random.random((5, 5, 3)) * 255).astype(np.uint8) # min input size
         mask = (np.random.random((5, 5, 3)) * 255).astype(np.uint8) # min input size
         w, h, c = frame.shape
-        for _ in carousel.catalog:
-            filtered_frame = carousel.current_filter.filter_frame(frame, mask)
-            p_w, p_h, p_c = filtered_frame.shape
-            assert w == p_w, f'wrong width in: {carousel.current_name}'
-            assert h == p_h, f'wrong height in: {carousel.current_name}'
-            assert c == p_c, f'wrong amount of color channels in: {carousel.current_name}'
-            carousel.next_filter()
+        filtered_frame = filter.filter_frame(frame, mask)
+        p_w, p_h, p_c = filtered_frame.shape
+        assert w == p_w, f'wrong width in: {name}'
+        assert h == p_h, f'wrong height in: {name}'
+        assert c == p_c, f'wrong amount of color channels in: {name}'
 
-    def test_filter_with_max_imagesize(self, carousel):
+    def test_filter_with_max_imagesize(self, filter: Filter, name: str):
         frame = (np.random.random((720, 1280, 3)) * 255).astype(np.uint8) # max camera resolution
         mask = (np.random.random((720, 1280, 3)) * 255).astype(np.uint8) # max camera resolution
         w, h, c = frame.shape
-        for _ in carousel.catalog:
-            filtered_frame = carousel.current_filter.filter_frame(frame, mask)
-            p_w, p_h, p_c = filtered_frame.shape
-            assert w == p_w, f'wrong width in: {carousel.current_name}'
-            assert h == p_h, f'wrong height in: {carousel.current_name}'
-            assert c == p_c, f'wrong amount of color channels in: {carousel.current_name}'
-            carousel.next_filter()
+        filtered_frame = filter.filter_frame(frame, mask)
+        p_w, p_h, p_c = filtered_frame.shape
+        assert w == p_w, f'wrong width in: {name}'
+        assert h == p_h, f'wrong height in: {name}'
+        assert c == p_c, f'wrong amount of color channels in: {name}'
 
-    def test_return_type(self, carousel):
+    def test_return_type(self, filter: Filter, name: str):
         frame = (np.random.random((100, 100, 3)) * 255).astype(np.uint8) 
         mask = (np.random.random((100, 100, 3)) * 255).astype(np.uint8)
-        for _ in carousel.catalog:
-            filtered_frame = carousel.current_filter.filter_frame(frame, mask)
-            assert isinstance(filtered_frame, np.ndarray), f'wrong return type in: {carousel.current_name}'
-            
+        filtered_frame = filter.filter_frame(frame, mask)
+        assert isinstance(filtered_frame, np.ndarray), f'wrong return type in: {name}'
