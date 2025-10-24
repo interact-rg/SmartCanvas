5cc1fa3f3978fb4557eb5e13b1530f3b159d6331
Marko Tiitto <Marko.Tiitto@student.oulu.fi>
2025-10-24 15:03:40 +0300

    add finger gun to core test

--- tests/core_test.py
+++ tests/core_test.py
@@ -1,7 +1,7 @@
 """ core_test.py """
 
 import pytest
-from smart_canvas.core import CanvasCore, Painting, Idle, Startup, Active
+from smart_canvas.core import CanvasCore, Painting, Idle, Startup, Active, ShowPic
 from smart_canvas.image_store import ImageStore
 from queue import Queue
 import cv2
@@ -17,6 +17,7 @@ abs_file_path_normal = os.path.join(script_dir, rel_path_normal)
 
 two_fingers = cv2.resize(cv2.imread(f"{abs_file_path_finger}/2.jpeg"), (1280,720), interpolation = cv2.INTER_AREA)
 five_fingers = cv2.resize(cv2.imread(f"{abs_file_path_finger}/5.jpeg"), (1280, 720), interpolation = cv2.INTER_AREA)
+finger_gun = cv2.resize(cv2.imread(f"{abs_file_path_finger}/finger_gun_test.png"), (1280, 720), interpolation = cv2.INTER_AREA)
 thumbs_down = cv2.resize(cv2.imread(f"{abs_file_path_finger}/thumbs_down_720.jpg"), (1280, 720), interpolation = cv2.INTER_AREA)
 face_neutral = cv2.resize(cv2.imread(f"{abs_file_path_normal}/neutral.png"), (1280, 720), interpolation = cv2.INTER_AREA)
 face_neutral_with_five_fingers = cv2.resize(cv2.imread(f"{abs_file_path_normal}/neutral_with_five_fingers.png"), (1280, 720), interpolation = cv2.INTER_AREA)
@@ -54,6 +55,13 @@ class TestCoreState:
             queue.put(five_fingers)
             time.sleep(0.1)
 
+        for _ in range(0,20):
+            print(f"finger gun")
+            print("\n")
+            
+            queue.put(finger_gun)
+            time.sleep(0.1)
+
         timeout = 0
         while type(core._state) is not type(Painting()) and timeout < 100:
             print(f"not painting")
@@ -61,5 +69,14 @@ class TestCoreState:
             timeout += 1
             time.sleep(0.1)
         assert type(core._state) is type(Painting())
+
+        while type(core._state) is type(Painting()) and timeout < 100:
+            print(f"painting")
+            queue.put(face_neutral)
+            timeout += 1
+            time.sleep(0.1)
+
+        assert type(core._state) is type(ShowPic())
+
 
 
