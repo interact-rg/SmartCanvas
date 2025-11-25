4cbd9e4431995c6f8638d76fe7efb3c6d9ff4096
Aleksi Backman <bddubbb@protonmail.com>
2025-11-25 18:03:14 +0200

    Reorder config variables

--- scripts/deploy_production/config.sh
+++ scripts/deploy_production/config.sh
@@ -1,7 +1,8 @@
+CONTAINER_REMOVE_ENABLED="FALSE"
+UPGRADE_PACKAGES="TRUE"
+
 # https://docs.csc.fi/cloud/pouta/additional-services/#custom-dns-name
 #
 # It is not recommended to use predefined fip-XXX... DNS names in
 # production.
 VIRTUAL_MACHINE_DNS_NAME="fip-86-50-20-216.kaj.poutavm.fi"
-CONTAINER_REMOVE_ENABLED="FALSE"
-UPGRADE_PACKAGES="TRUE"
