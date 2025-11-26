2868ce9fbf40bb042a503c5d63d98dc30a2c0e1f
Aleksi Backman <pottumuusi@users.noreply.github.com>
2025-11-26 23:47:38 +0200

    Introduce production deployment scripts (#34)
    Add scripts that can be used for starting to serve SmartCanvas application on a production cPouta virtual machine.
    
    The added documentation includes commands for generating 3 management scripts with the effect of:
    1. Updating deployed source code
    2. Starting application
    3. Stopping application
    
    These management scripts can be executed on a new host straight after creating it.
    
    The behavior of `scripts/deploy_production/production_start.sh` can be affected to a degree by modifying `scripts/deploy_production/config.sh`. At least `VIRTUAL_MACHINE_DNS_NAME` will need to be set to match the virtual machine running the scripts. The default config in currently contains the domain name of a testing virtual machine.

---
+++ scripts/deploy_production/config.sh
@@ -0,0 +1,12 @@
+readonly CONTAINER_REMOVE_ENABLED="TRUE"
+readonly IMAGE_REMOVE_ENABLED="TRUE"
+readonly UPGRADE_PACKAGES="FALSE"
+
+# See variant options from constants.sh
+readonly FRONTEND_BUILD_VARIANT="${BUILD_VARIANT_CONSENT_ON}"
+
+# https://docs.csc.fi/cloud/pouta/additional-services/#custom-dns-name
+#
+# It is not recommended to use predefined fip-XXX... DNS names in
+# production.
+readonly VIRTUAL_MACHINE_DNS_NAME="fip-86-50-20-216.kaj.poutavm.fi"
---
+++ scripts/deploy_production/production_stop.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+cd $(dirname $0)
+
+stop_docker_containers() {
+	local containers_json=""
+	local container_ids=""
+
+	containers_json="$(sudo docker container ps -a --format json)"
+	if [ "" != "${containers_json}" ] ; then
+		echo "Stopping all Docker containers"
+		container_ids="$(echo ${containers_json} \
+			| jq '.ID' \
+			| tr '\n' ' ' \
+			| tr -d \")"
+		sudo docker container stop ${container_ids} \
+			|| error_exit "Failed to stop docker containers: ${container_ids}"
+	fi
+}
+
+main() {
+	source ./common.sh
+
+	echo "Stopping application"
+
+	stop_docker_containers
+
+	terminate_running_caddy
+}
+
+main "${@}"
