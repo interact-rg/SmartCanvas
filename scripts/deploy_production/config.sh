6c031212c457e88437618d547c5e1ff59f6040ba
Aleksi Backman <bddubbb@protonmail.com>
2025-11-26 21:52:58 +0200

    Make config variables readonly

--- scripts/deploy_production/config.sh
+++ scripts/deploy_production/config.sh
@@ -1,12 +1,12 @@
-CONTAINER_REMOVE_ENABLED="TRUE"
-IMAGE_REMOVE_ENABLED="TRUE"
-UPGRADE_PACKAGES="FALSE"
+readonly CONTAINER_REMOVE_ENABLED="TRUE"
+readonly IMAGE_REMOVE_ENABLED="TRUE"
+readonly UPGRADE_PACKAGES="FALSE"
 
 # See variant options from constants.sh
-FRONTEND_BUILD_VARIANT="${BUILD_VARIANT_CONSENT_ON}"
+readonly FRONTEND_BUILD_VARIANT="${BUILD_VARIANT_CONSENT_ON}"
 
 # https://docs.csc.fi/cloud/pouta/additional-services/#custom-dns-name
 #
 # It is not recommended to use predefined fip-XXX... DNS names in
 # production.
-VIRTUAL_MACHINE_DNS_NAME="fip-86-50-20-216.kaj.poutavm.fi"
+readonly VIRTUAL_MACHINE_DNS_NAME="fip-86-50-20-216.kaj.poutavm.fi"
