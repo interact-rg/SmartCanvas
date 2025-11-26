085eee1d2a2d0a99e0063ffbde7ee0f069c6da8d
Aleksi Backman <bddubbb@protonmail.com>
2025-11-26 13:03:06 +0200

    Add comment

--- scripts/deploy_production/config.sh
+++ scripts/deploy_production/config.sh
@@ -1,5 +1,7 @@
 CONTAINER_REMOVE_ENABLED="FALSE"
 UPGRADE_PACKAGES="TRUE"
+
+# See variant options from constants.sh
 FRONTEND_BUILD_VARIANT="${BUILD_VARIANT_CONSENT_ON}"
 
 # https://docs.csc.fi/cloud/pouta/additional-services/#custom-dns-name
