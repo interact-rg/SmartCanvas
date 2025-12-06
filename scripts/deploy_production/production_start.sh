e77c057cc8b3a9969e15177309886e68f93c0598
Aleksi Backman <bddubbb@protonmail.com>
2025-12-06 10:45:31 +0200

    Propagate environment variable used in endpoint obfuscation
    The changes result in SecretsUsedInArgOrEnv warning being generated when
    building backend image. This becomes an issue if the image gets
    published to a registry. The endpoint path can then be discovered from
    the image. To reduce the visibility of endpoint path, pass it to
    container by using bind mounts.
    
    Created #37 for fixing SecretsUsedInArgOrEnv warnings.

--- scripts/deploy_production/production_start.sh
+++ scripts/deploy_production/production_start.sh
@@ -17,6 +17,10 @@ validate_config() {
 	if [ "" == "${VIRTUAL_MACHINE_DNS_NAME}" ] ; then
 		error_exit "Please set a value for VIRTUAL_MACHINE_DNS_NAME in config.sh"
 	fi
+
+	if [ "" == "${BACKEND_APP_ENDPOINT}" ] ; then
+		error_exit "Please set a value for BACKEND_APP_ENDPOINT in config.sh"
+	fi
 }
 
 install_dependencies() {
@@ -159,6 +163,7 @@ build_backend_docker_image() {
 		echo "Building backend Docker image"
 		sudo docker build \
 			--file Dockerfile.backend \
+			--build-arg APP_ENDPOINT_TOKEN=${BACKEND_APP_ENDPOINT} \
 			-t "${DOCKER_IMAGE_BACKEND}" \
 			. \
 			|| error_exit "Failed to build backend Docker image"
