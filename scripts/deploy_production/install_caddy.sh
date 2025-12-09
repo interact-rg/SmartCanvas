5586cd7329d48e44827022f1a5e2a2a1f842ea12
Aleksi Backman <pottumuusi@users.noreply.github.com>
2025-12-09 20:29:29 +0200

    Production deployment fixes 2025-12-09 (#42)

--- scripts/deploy_production/install_caddy.sh
+++ scripts/deploy_production/install_caddy.sh
@@ -12,8 +12,8 @@ sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https cu
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
 
-chmod o+r /usr/share/keyrings/caddy-stable-archive-keyring.gpg
-chmod o+r /etc/apt/sources.list.d/caddy-stable.list
+sudo chmod o+r /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+sudo chmod o+r /etc/apt/sources.list.d/caddy-stable.list
 
 sudo apt update
 sudo apt -y install caddy
